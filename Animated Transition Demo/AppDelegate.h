a50bf74f4f32dc4b36e45196adf0062c34c0ec91
Joshua Sullivan <jsulliva@nerdery.com>
2015-03-19 14:48:55 -0500

    Initial Commit

---
+++ Animated Transition Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Animated Transition Demo
+//
+//  Created by Joshua Sullivan on 3/19/15.
+//  Copyright (c) 2015 Joshua Sullivan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Animated Transition Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Animated Transition Demo
+//
+//  Created by Joshua Sullivan on 3/19/15.
+//  Copyright (c) 2015 Joshua Sullivan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
